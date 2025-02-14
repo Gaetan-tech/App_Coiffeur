366febcbb69b5472175def44c779733eaa224f7e
Gaetan <gaetan.fomat@ynov.com>
2025-02-14 05:53:36 -0800

    first commit

---
+++ src/main/java/org/example/app_coiffure/AppCoiffureApplication.java
@@ -0,0 +1,13 @@
+package org.example.app_coiffure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppCoiffureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppCoiffureApplication.class, args);
+    }
+
+}
